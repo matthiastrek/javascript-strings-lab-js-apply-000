78ec3b36a767bc15c19eac14fd775946a3d27ac3
Matthew Trenk <matt.trenk@gmail.com>
2018-01-26 16:30:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -7,3 +7,7 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+console.log('Hello, ' + specialGuest + '!')
+
+console.log
